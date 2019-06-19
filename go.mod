db9e0c7d3b3b82b8cc7a23a2cc55e977f048e2dc
Andreas Bergmeier <abergmeier@gmx.net>
2019-06-19 22:16:27 +0200

    Try to implement authentication.

--- go.mod
+++ go.mod
@@ -7,21 +7,22 @@ require (
 	github.com/Microsoft/hcsshim v0.8.6 // indirect
 	github.com/containerd/continuity v0.0.0-20190426062206-aaeac12a7ffc // indirect
 	github.com/docker/cli v0.0.0-20190613135022-d7f806fa0023
+	github.com/docker/distribution v2.7.1+incompatible
 	github.com/docker/docker v0.0.0-20190613135022-3998dffb806f3887f804b813069f59bc14a7f3c1 // indirect
 	github.com/docker/docker-credential-helpers v0.6.2 // indirect
+	github.com/docker/go-metrics v0.0.0-20181218153428-b84716841b82 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
-	github.com/gogo/protobuf v1.1.1 // indirect
 	github.com/google/go-cmp v0.2.0 // indirect
 	github.com/gorilla/mux v1.7.1
-	github.com/json-iterator/go v1.1.6 // indirect
 	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/opencontainers/runc v0.1.1 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/prometheus/client_golang v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.4.2 // indirect
 	github.com/spf13/pflag v1.0.3 // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
 	golang.org/x/net v0.0.0-20190311183353-d8887717615a // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 	gotest.tools v2.2.0+incompatible // indirect
 	k8s.io/api v0.0.0-20190425012535-181e1f9c52c1 // indirect
--- internal/backing/backing.go
+++ internal/backing/backing.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+	"net/url"
 
-	"github.com/otto-de/ohammer/internal/config"
 	"github.com/docker/cli/cli/config/credentials"
+	"github.com/otto-de/ohammer/internal/config"
+	"golang.org/x/oauth2"
 )
 
 // Poll checks whether a certain result is available on the redirection target
@@ -24,16 +26,35 @@ func Poll(req *http.Request, sectionPath string, t *config.Target) (*http.Respon
 
 	switch req.Method {
 	case "GET":
-		return pollGet(req, store, backingURL.String())
+		return pollGet(req, store, backingURL)
 	case "HEAD":
-		return pollHead(req, store, backingURL.String())
+		return pollHead(req, store, backingURL)
 	}
 
 	panic("NOT IMPLEMENTED YET")
 }
 
-func pollGet(req *http.Request, store credentials.Store, url string) (*http.Response, error) {
-	req, err := http.NewRequest("GET", url, bytes.NewBuffer([]byte{}))
+func authenticate(req *http.Request, store credentials.Store, url *url.URL) error {
+	hostAuth, err := store.Get(url.Host)
+	if err != nil {
+		return err
+	}
+	if hostAuth.RegistryToken == "" {
+		return nil
+	}
+	token := oauth2.Token{
+		AccessToken: hostAuth.RegistryToken,
+	}
+	token.SetAuthHeader(req)
+	return nil
+}
+
+func pollGet(req *http.Request, store credentials.Store, url *url.URL) (*http.Response, error) {
+	req, err := http.NewRequest("GET", url.String(), bytes.NewBuffer([]byte{}))
+	if err != nil {
+		panic(err)
+	}
+	err = authenticate(req, store, url)
 	if err != nil {
 		panic(err)
 	}
@@ -43,8 +64,12 @@ func pollGet(req *http.Request, store credentials.Store, url string) (*http.Resp
 	return resp, err
 }
 
-func pollHead(req *http.Request, store credentials.Store, url string) (*http.Response, error) {
-	req, err := http.NewRequest("HEAD", url, bytes.NewBuffer([]byte{}))
+func pollHead(req *http.Request, store credentials.Store, url *url.URL) (*http.Response, error) {
+	req, err := http.NewRequest("HEAD", url.String(), bytes.NewBuffer([]byte{}))
+	if err != nil {
+		panic(err)
+	}
+	err = authenticate(req, store, url)
 	if err != nil {
 		panic(err)
 	}
